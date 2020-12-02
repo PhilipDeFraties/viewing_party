c10bd5cba405325f66b4fc895093a85b276f1a8b
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-02 15:33:26 -0700

    refactor create action in friendship controller to call model method on current_user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,7 @@ class FriendshipsController < ApplicationController
   def create
     new_friend = User.find_by(email: params[:friends_email])
     if new_friend && (current_user.email != params[:friends_email])
-      new_friend.create_friendships(current_user.id, new_friend.id)
+      current_user.create_friendships(current_user.id, new_friend.id)
     elsif current_user.email == params[:friends_email]
       flash[:error] = 'You cannot add yourself as a friend.'
     else
