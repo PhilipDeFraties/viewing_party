5be7d7de09a9de11606e06b0b5828070727a40ad
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-01 20:50:15 -0700

    Change spacing
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
-  
+
   post "/login", to: "sessions#create"
 
-  get '/dashboard', to: 'users#show'
+  namespace :user do 
+    get '/dashboard', to: 'dashboard#show'
+  end 
 
   get '/discover', to: 'movies#search'
+
+  post '/friendship/create', to: 'friendships#create'
 end
