764e381d7eb9b770b2db414dea89f52f2a83e320
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-02 15:53:53 -0700

    Create controller for discover page and its features
    Co-authored-by: Hanna Davis <hannadavis09@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,16 @@
 class MoviesController < ApplicationController
+
+  def top40
+
+  end
+
   def search
+    search = params[:search]
+
+    redirect_to "/movies/results?q=#{search.downcase}"
+  end
+
+  def search_results
 
   end
 end
