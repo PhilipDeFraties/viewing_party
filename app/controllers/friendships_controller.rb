54fa142af2d9d88e46048c0af90ff9cb398e5819
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-02 16:13:22 -0700

    Refactors conditional within freindship_controller create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,10 @@
 class FriendshipsController < ApplicationController
   def create
     new_friend = User.find_by(email: params[:friends_email])
-    if new_friend && (current_user.email != params[:friends_email])
-      current_user.create_friendships(current_user.id, new_friend.id)
-    elsif current_user.email == params[:friends_email]
+    if current_user.email == params[:friends_email]
       flash[:error] = 'You cannot add yourself as a friend.'
+    elsif new_friend
+      current_user.create_friendships(current_user.id, new_friend.id)
     else
       flash[:error] = 'User does not exist.'
     end
