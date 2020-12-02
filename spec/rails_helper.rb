779c750b4e6e6ff40e2117647e7cc11063d52b58
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-01 17:39:54 -0700

    Adds factory_bot config to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -56,7 +57,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.include Capybara::DSL
-  
+
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
