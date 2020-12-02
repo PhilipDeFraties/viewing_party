e2ed4e4ab77e8ac99be4252ab5e471a7c57df432
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-01 20:55:30 -0700

    Update factory for failing tests
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password_digest  { Faker::String.random(length: 4) }
+    password  { 'password' }
+    password_confirmation  { 'password' }
     username { Faker::Name.name  }
   end
 end
