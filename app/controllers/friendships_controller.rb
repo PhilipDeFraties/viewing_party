3a34c1b210cc18b6706c2db02f984883f0f53250
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-01 20:44:55 -0700

    Add friendship create method

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,16 @@
+class FriendshipsController < ApplicationController
+  def create
+    new_friend = User.find_by(email: params[:friends_email])
+    if new_friend && (current_user.email != params[:friends_email])
+      Friendship.create(user_id: current_user.id,
+                        friend_id: new_friend.id)
+      redirect_to '/user/dashboard'
+    elsif current_user.email == params[:friends_email]
+      flash[:error] = 'You cannot add yourself as a friend.'
+      redirect_to '/user/dashboard'
+    else 
+      flash[:error] = 'User does not exist.'
+      redirect_to '/user/dashboard'
+    end
+  end
+end
