c4f23681d701719cb98d5ab638b8a01ba1348e2f
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-01 20:48:08 -0700

    Add relationships and user_status method
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,14 @@
 class Party < ApplicationRecord
   belongs_to :user
+  belongs_to :movie
+  has_many :party_guests
+  has_many :users, through: :party_guests
+
+  def user_status(user_id)
+    if self.user_id == user_id
+      'Host'
+    else
+      'Invited'
+    end
+  end
 end
