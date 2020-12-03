85963d7acc26ab1d0acefe72b0f75ec5191eedbe
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-02 20:45:34 -0700

    Rubocop auto-corrects

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,12 @@
 class MoviesController < ApplicationController
-  def show
-
-  end
+  def show; end
 
   def top40
-    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["THE_MOVIE_DB_API"]}&language=en-US")
+    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
     response = conn.get
     json = JSON.parse(response.body, symbolize_names: true)
 
-    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["THE_MOVIE_DB_API"]}&language=en-US&page=2")
+    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&page=2")
 
     response2 = conn2.get
     json2 = JSON.parse(response2.body, symbolize_names: true)
