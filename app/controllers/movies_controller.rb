7ef74575a6135c3a6fa829d0d0395b976acbc174
GregJMitchell <g2mitchell3003@gmail.com>
2020-12-02 18:19:19 -0700

    Create top40 action that calls the API for the top 40 movies
    Co-authored-by: Hanna Davis <hannadavis09@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,19 @@
 class MoviesController < ApplicationController
-  def top40; end
+  def show
+    
+  end
+
+  def top40
+    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["THE_MOVIE_DB_API"]}&language=en-US")
+    response = conn.get
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["THE_MOVIE_DB_API"]}&language=en-US&page=2")
+    
+    response2 = conn2.get
+    json2 = JSON.parse(response2.body, symbolize_names: true)
+    @top_40 = [json[:results], [json2[:results]]].flatten
+  end
 
   def search
     search = params[:search]
