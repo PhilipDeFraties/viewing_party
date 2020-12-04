2686148bd51475d6ec61755ef356642ae6f547d3
Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
2020-12-04 16:27:49 -0700

    Movie details (#38)
    * Add webmock and vcr
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Remove search_results method, change top40 variable name, and make search method
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Remove unnecessary view after refactor
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add view for search results
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Change top40 variable name to normalcase
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Refactor routes to remove unnecessary views
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add webmock and vcr
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add vcr to tests
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add tests for search results page
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add vcr to tests
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add cassettes for tests
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Create show method
    
    * Add movie_id to params for movie show page
    
    * WIP vcr cassettes
    
    * Fix VCR testing issues
    
    * Fix VCR testing issues
    
    * Add view for movie details page
    
    * Change instance variable name
    
    * Add styling for sections, WIP add party button
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add new method
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add view for movie show page
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add empty view for new party page
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add route for new party
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Update tests with VCR
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add VCR yml files
    
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add styling for movie show page
    
    Co-authored-by: Hanna Davis <hanna.e.davis@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,19 @@
 class MoviesController < ApplicationController
-  def show; end
+  def show
+    details_conn = Faraday.new("https://api.themoviedb.org/3/movie/#{params[:movie_id]}?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
+    details_response = details_conn.get
+    details = JSON.parse(details_response.body, symbolize_names: true)
+
+    cast_conn = Faraday.new("https://api.themoviedb.org/3/movie/#{params[:movie_id]}/credits?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
+    cast_response = cast_conn.get
+    cast = JSON.parse(cast_response.body, symbolize_names: true)
+
+    reviews_conn = Faraday.new("https://api.themoviedb.org/3/movie/#{params[:movie_id]}/reviews?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
+    reviews_response = reviews_conn.get
+    reviews = JSON.parse(reviews_response.body, symbolize_names: true)
+    
+    @results = { :details => details, :cast => cast, :reviews => reviews}
+  end
 
   def top40
     conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
