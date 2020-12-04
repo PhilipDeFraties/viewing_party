d14510b6dfd97a67466128cd4853b4dceda8e94c
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-03 20:37:03 -0700

    Update tests with VCR
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- spec/features/movies/search_spec.rb
+++ spec/features/movies/search_spec.rb
@@ -1,32 +1,56 @@
 require 'rails_helper'
-
 RSpec.describe "Movie Search Results page" do
   describe "As a registered user, when I visit the discover page and fill in the form" do
     before :each do 
       @user_1 = create :user
-
       visit '/'
       fill_in :email, with: @user_1.email
       fill_in :password, with: @user_1.password
       click_button 'Login'
       visit '/discover'
+      VCR.insert_cassette('parasite_search')
     end
-
+    after :each do
+      VCR.eject_cassette('parasite_search')
+    end
     it 'I search for a movie and it shows the 17 results on the /movie/resutls page' do
-      VCR.use_cassette('search_jurassic_park') do
-        fill_in :search, with: 'Jurassic Park'
-        click_on 'Find Movies'
-        expect(current_path).to eq('/movies/search')
-        within('#movies-table') do
-          expect(all('tr').count).to eq(17)
-        end
-     end
+      fill_in :search, with: 'Parasite'
+      click_on 'Find Movies'
+      expect(current_path).to eq('/movies/search')
+      within('#movies-table') do
+        expect(all('tr').count).to eq(20)
+      end
     end
-
     it 'I see the rating for each movie and each movie title is a link to its show page' do
-      VCR.use_cassette('search_jurassic_park') do
-        fill_in :search, with: 'Jurassic Park'
+      fill_in :search, with: 'Parasite'
+      click_on 'Find Movies'
+      expect(current_path).to eq('/movies/search')
+      expect(page).to have_css('.rating')
+      expect(page).to have_css('.title')
+      rating = all('.rating')
+      title = all('.title')
+      expect(rating).not_to be_empty
+      expect(title).not_to be_empty
+    end
+    it "When I click on a movies link, I am taken to its show page" do
+      fill_in :search, with: 'Parasite'
+      click_on 'Find Movies'
+      expect(current_path).to eq('/movies/search')
+      within first('.title') do
+        VCR.use_cassette('parasite_details') do
+          click_link
+        end
+      end
+    end
+    it "I can search for another movie" do
+      fill_in :search, with: 'Parasite'
+      click_on 'Find Movies'
+      VCR.use_cassette('harry_search') do
+        fill_in :search, with: 'Harry'
         click_on 'Find Movies'
+        within('#movies-table') do
+          expect(all('tr').count).to eq(20)
+        end
         expect(current_path).to eq('/movies/search')
         expect(page).to have_css('.rating')
         expect(page).to have_css('.title')
@@ -34,38 +58,6 @@ RSpec.describe "Movie Search Results page" do
         title = all('.title')
         expect(rating).not_to be_empty
         expect(title).not_to be_empty
-      end
-    end
-
-    it "When I click on a movies link, I am taken to its show page" do
-      VCR.use_cassette('search_jurassic_park') do
-        fill_in :search, with: 'Jurassic Park'
-        click_on 'Find Movies'
-        expect(current_path).to eq('/movies/search')
-        within first('.title') do
-          click_link
-        end
-      end
-    end
-
-    it "I can search for another movie" do
-      VCR.use_cassette('search_jurassic_park') do
-        fill_in :search, with: 'Jurassic Park'
-        click_on 'Find Movies'
-        VCR.use_cassette('search_parasite') do
-          fill_in :search, with: 'Parasite'
-          click_on 'Find Movies'
-          within('#movies-table') do
-            expect(all('tr').count).to eq(20)
-          end
-          expect(current_path).to eq('/movies/search')
-          expect(page).to have_css('.rating')
-          expect(page).to have_css('.title')
-          rating = all('.rating')
-          title = all('.title')
-          expect(rating).not_to be_empty
-          expect(title).not_to be_empty
-        end
       end
     end
   end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
-
 RSpec.describe "Movie Details page" do
   describe "As a registered user, when I visit a movies detail page" do
     before :each do 
       @user_1 = create :user
-
       visit '/'
       fill_in :email, with: @user_1.email
       fill_in :password, with: @user_1.password
@@ -12,54 +10,44 @@ RSpec.describe "Movie Details page" do
       visit '/discover'
       VCR.insert_cassette('jurassic_search')
     end
-
     after :each do
       VCR.eject_cassette('jurassic_search')
     end
-
     it 'I see a button to create a viewing party' do
-      
         fill_in :search, with: 'Jurassic Park'
         click_on 'Find Movies'
         expect(current_path).to eq('/movies/search')
         within first('.title') do
           click_link
         end
-      
-      
         expect(current_path).to eq('/movies/329')
         expect(page).to have_button('Create Viewing Party for Movie')
         click_on 'Create Viewing Party for Movie'
         expect(current_path).to eq('/viewing-party/new')
-      
     end
-
     it 'I see a button to create a viewing party' do
-      
-        fill_in :search, with: 'Jurassic Park'
-        click_on 'Find Movies'
-        expect(current_path).to eq('/movies/search')
-        within first('.title') do
-          click_link
-        end
-      
-      
-        
-          
-            expect(page).to have_css('#title')
-            expect(page).to have_content('Jurrasic Park')
-            expect(page).to have_css('#vote-average')
-            expect(page).to have_content('Vote Average: 6.0')
-            expect(page).to have_css('#runtime')
-            expect(page).to have_content('Runtime: 1hr 54min')
-            expect(page).to have_css('#genre')
-            expect(page).to have_content("Genre(s): ")
-            expect(page).to have_css('#description')
-            expect(page).to have_content('')
-            expect(page).to have_css('#cast')
-            expect(page).to have_content('')
-            expect(page).to have_css('#review')
-            expect(page).to have_content('')
+      fill_in :search, with: 'Jurassic Park'
+      click_on 'Find Movies'
+      expect(current_path).to eq('/movies/search')
+      within first('.title') do
+        click_link
+      end
+      expect(page).to have_css('#title')
+      expect(page).to have_content('Jurassic Park')
+      expect(page).to have_css('#vote-average')
+      expect(page).to have_content('Vote Average: 7.9')
+      expect(page).to have_css('#runtime')
+      expect(page).to have_content('Runtime: 2hrs 7mins')
+      expect(page).to have_css('#genre')
+      expect(page).to have_content("Genre(s): ")
+      expect(page).to have_css('#description')
+      expect(page).to have_content('A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs')
+      expect(page).to have_css('#cast')
+      expect(page).to have_content('Sam Neill as Dr. Alan Grant')
+      expect(page).to have_css('#review')
+      expect(page).to have_content('4 Reviews')
+      expect(page).to have_content('Author: BinaryCrunch')
+      expect(page).to have_content('If you somehow missed this movie and have never seen it then watch it immediately.')
     end
   end
 end
