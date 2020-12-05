cc79a077c55d79092d8e0e58f841a84576c7306b
Greg Mitchell <67349568+GregJMitchell@users.noreply.github.com>
2020-12-05 14:43:19 -0700

    Refactor sessions controller create action (#45)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if !user
-      flash[:error] = 'Email and/or password is incorrect'
-      redirect_to '/'
-    elsif user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/user/dashboard'
       flash[:success] = 'You are now logged in'
