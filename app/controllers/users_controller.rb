9e7791fbca1af39061f305f61a1bfd5a571feeb1
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-04 17:42:24 -0700

    Add users_controller with new and create methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    if !current_user.nil?
+      flash[:notice] = 'You are already registerd.'
+      redirect_to '/user/dashboard'
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.username}!"
+      redirect_to '/user/dashboard'
+    else
+      generate_flash(@user)
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
