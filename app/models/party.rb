c9a0aae4aac485c29c5b016a8ba77f54d2ff0706
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-05 14:16:02 -0700

    Move invite_friends method from parties controller

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,12 +3,20 @@ class Party < ApplicationRecord
   belongs_to :movie
   has_many :party_guests, dependent: :destroy
   has_many :users, through: :party_guests
+  # validates_length_of :date, :is => 10
 
   def user_status(user_id)
     if self.user_id == user_id
       'Host'
     else
       'Invited'
+    end
+  end
+
+  def invite_friends(party_guests)
+    party_guests.reject! { |pg| pg.empty? }
+    party_guests.each do |guest|
+      self.party_guests.create(user_id: guest.to_i)
     end
   end
 end
