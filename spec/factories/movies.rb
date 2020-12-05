d2e1d79c39cb46d2ce4fc1f2dff22abf609797e9
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-05 14:36:10 -0700

    Adds unique number to faker number generator in movie factory

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :movie do
     title { Faker::Movie.title }
     runtime  { 90 }
-    api_id { Faker::Number.number(digits: 4) }
+    api_id { Faker::Number.unique.number(digits: 4) }
     logo { "/astKJpagcTTqybiAZ6qpakVqmow.jpg" }
   end
 end
