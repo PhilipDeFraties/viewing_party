2b504b2d7e915d4e0086ee9a1c4554f24f519842
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2020-12-05 14:44:31 -0700

    Model specs (#44)
    * Add bcrypt gem
    
    * Setup flash messages
    
    * Add style for flash messages
    
    * Add Shoulda Matchers to gemfile
    
    * Add Shoulda Matchers to RSpec config
    
    * Create test for user logging in
    
    * Create sessions controller
    
    * Create route for login
    
    * Change host_id to user_id in parties table
    
    * Add relationship and validation for users and parties
    
    * Move welcome page into features directory
    
    * Change password field to be a password_field_tag
    
    * Create tests for user validations and relationships
    
    * Add test for welcome message
    
    * Add test for discover page button
    
    * Add tests for adding friends
    
    * Add test for showing viewing parties
    
    * Add welcome message
    
    * Add discover movies button
    
    * Add dashboard route
    
    * Add method for users show page
    
    * WIP movies discover route
    
    * WIP elastic search for friends
    
    * WIP empty search show page
    
    * WIP empty search method
    
    * WIP elastic search friends method
    
    * Adds factory_bot and faker gems
    
    * Adds user and movie factories
    
    * Adds factory_bot config to rails_helper
    
    * Adds factory_bot config to rails helper
    
    * Move file to feature tests
    
    * Add test for elastic search
    
    * WIP elastic search
    
    * Inherit from applicationcontroller
    
    * Fix CI issues
    
    * Add authentication
    
    * Add friendship create method
    
    * Delete moved file
    
    * redirect to new namesapaced controller
    
    * Add empty show method
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add has many parties relationship
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add relationships and user_status method
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add party guests method
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Remove pry
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add dashboard view
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Delete file
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Change spacing
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Add relationships
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Update schema after migration
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Change factory that was failing tests
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Update factory for failing tests
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Update tests for non-elastic tests
    
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>
    
    * Remove generate_flash action from application_controller
    
    * Adds validations to models
    
    * Add faker number generator to movie factory
    
    * Removes old dashboard spec file
    
    * Adds model tests for friendship, movie, and party_guest
    
    * removes dashboard controller because it keeps renaming the directory
    
    * Adds new dashboard controller
    
    * Adds unique number to faker number generator in movie factory
    
    Co-authored-by: GregJMitchell <g2mitchell3003@gmail.com>
    Co-authored-by: Hanna Davis <hanna.e.davis@gmail.com>
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.coom>

--- app/models/party_guest.rb
+++ app/models/party_guest.rb
@@ -1,4 +1,6 @@
 class PartyGuest < ApplicationRecord
+  validates :user_id, :party_id, presence: true
+
   belongs_to :user
   belongs_to :party
 end
--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :movie do
     title { Faker::Movie.title }
     runtime  { 90 }
-    api_id { 123456 }
+    api_id { Faker::Number.unique.number(digits: 4) }
     logo { "/astKJpagcTTqybiAZ6qpakVqmow.jpg" }
   end
 end
