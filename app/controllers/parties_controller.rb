716d5145588e64c04052f4391ad01a2742b5a3f2
Hanna Davis <hanna.e.davis@gmail.com>
2020-12-04 20:30:48 -0700

    Make create method

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -4,30 +4,35 @@ class PartiesController < ApplicationController
   end
 
   def create
-    require 'pry'; binding.pry
-    @party = Party.new(party_params)
+    if Movie.find_by(api_id: params[:api_id])
+      movie = Movie.find_by(api_id: params[:api_id])
+    else
+      movie = Movie.create!(movie_params)
+    end
+    @party = current_user.parties.new(
+      date: params[:date],
+      time: params[:time],
+      duration: params[:duration],
+      movie_id: movie.id)
     if @party.save
-      if Movie.find_by(api_id: params[:api_id])
-        flash[:success] = 'Your Viewing Party has been Created'
-        redirect_to '/user/dashboard'
-      else
-          Movie.create(movie_params)
-          flash[:success] = 'Your Viewing Party has been Created'
-          redirect_to '/user/dashboard'
-      end
+      invite_friends(params[:party_guests])
+      flash[:success] = 'Your Viewing Party was created!'
+      redirect_to '/user/dashboard'
     else
       flash[:error] = @party.errors.full_messages.to_sentence
       render :new
     end
   end
 
+  def invite_friends(party_guests)
+    party_guests.each do |guest|
+      @party.party_guests.create(user_id: guest.to_i)
+    end
+  end
+
   private
 
-  # def party_params
-  #   params.require(:party).permit(:user_id, :movie_id, :date, :time, :duration)
-  # end
-
-  # def movie_params
-  #   params.permit(:title, :runtime, :api_id, :logo)
-  # end
+  def movie_params
+    params.permit(:title, :runtime, :api_id, :logo)
+  end
 end
