9c55f8e42a029982f94a85e5026d5dcf480ba8d5
Philip DeFraties <philipjfry338@gmail.coom>
2020-12-06 11:36:24 -0700

    Adds user edit view

---
+++ spec/features/user/edit_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'User edit' do
+  describe 'As an authenticated user, when I visit my dashboard page' do
+    before(:each) do
+      @user_1 = create :user
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+    end
+
+    
+  end
+end
