b1fcd012040824a894eb50da52231587f56305f2
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 11:52:35 -0700

    Add top40 and search class methods
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -3,4 +3,18 @@ class MovieFacade
     json = MovieService.movie_details(movie_id)
     @movie = MovieDetails.new(json)
   end
+
+  def self.top40
+    json = MovieService.top40
+    json.map do |movie|
+      MovieResults.new(movie)
+    end
+  end
+
+  def self.search(search)
+    json = MovieService.search(search)
+    json.map do |movie|
+      MovieResults.new(movie)
+    end
+  end
 end
