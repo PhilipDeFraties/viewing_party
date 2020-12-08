f5b9c7270509e604f800fe849d212941d8b86d59
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 11:53:11 -0700

    Add top40 and search methods
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -4,4 +4,22 @@ class MovieService
     response = conn.get
     json = JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.top40
+    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
+    response = conn.get
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&page=2")
+    response2 = conn2.get
+    json2 = JSON.parse(response2.body, symbolize_names: true)
+    [json[:results], [json2[:results]]].flatten
+  end
+
+  def self.search(search)
+    conn = Faraday.new("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&query=#{search}%20")
+    response = conn.get
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:results]
+  end
 end
