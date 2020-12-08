19663241c56cc1ba4c5c0d0fda2e33ede8a4cf36
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 11:52:18 -0700

    Abstract api call out of controller
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,22 +4,11 @@ class MoviesController < ApplicationController
   end
 
   def top40
-    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
-    response = conn.get
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&page=2")
-
-    response2 = conn2.get
-    json2 = JSON.parse(response2.body, symbolize_names: true)
-    @top40 = [json[:results], [json2[:results]]].flatten
+    @top40 = MovieFacade.top40
   end
 
   def search
-    conn = Faraday.new("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&query=#{params[:search]}%20")
-    response = conn.get
-    json = JSON.parse(response.body, symbolize_names: true)
-    @results = json[:results]
+    @results = MovieFacade.search(params[:search])
     flash[:error] = 'No movies matched your search.' if @results.empty?
   end
 end
