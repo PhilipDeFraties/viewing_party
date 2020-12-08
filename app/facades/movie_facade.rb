1a33061ec013d2c2c1279bd88181fea593b49786
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 10:28:04 -0700

    Create movie facade
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,8 +1,6 @@
 class MovieFacade
   def self.details(movie_id)
-    conn = Faraday.new("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&to_response=credits%2Creviews")
-    response = conn.get
-    parse = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    json = MovieService.movie_details(movie_id)
+    @movie = MovieDetails.new(json)
   end
 end
