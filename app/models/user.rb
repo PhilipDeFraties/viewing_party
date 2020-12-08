2db8b5958ddc55dc75cebb0bde04467606c3e280
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 16:43:33 -0700

    WIP refactor find_parties method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,10 @@ class User < ApplicationRecord
   has_many :parties, dependent: :destroy
 
   def find_parties
+    # array = []
+    # array << Party.where(id: PartyGuest.where(user_id: id).pluck(:party_id))
+    # array << parties
+    # array.flatten
     Party.where(user_id: id).or(Party.where(id: PartyGuest.where(user_id: id).pluck(:party_id)))
   end
 
