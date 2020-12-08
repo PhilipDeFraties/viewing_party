89c0b6797b794229d94f1ae9d18814369062c2e6
oxalisviolacea <hannadavis09@gmail.com>
2020-12-08 13:41:31 -0700

    Add helper method for api calls
    Co-authored-by: Philip DeFraties <philipjfry338@gmail.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,25 +1,27 @@
 class MovieService
   def self.movie_details(movie_id)
-    conn = Faraday.new("https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&append_to_response=credits%2Creviews")
-    response = conn.get
-    json = JSON.parse(response.body, symbolize_names: true)
+    uri = "/3/movie/#{movie_id}?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&append_to_response=credits%2Creviews"
+    get_results(uri)
   end
 
   def self.top40
-    conn = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US")
-    response = conn.get
-    json = JSON.parse(response.body, symbolize_names: true)
+    uri = "/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US"
+    json = get_results(uri)
 
-    conn2 = Faraday.new("https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&page=2")
-    response2 = conn2.get
-    json2 = JSON.parse(response2.body, symbolize_names: true)
+    uri = "/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&page=2"
+    json2 = get_results(uri)
     [json[:results], [json2[:results]]].flatten
   end
 
   def self.search(search)
-    conn = Faraday.new("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&query=#{search}%20")
-    response = conn.get
-    json = JSON.parse(response.body, symbolize_names: true)
+    uri = "/3/search/movie?api_key=#{ENV['THE_MOVIE_DB_API']}&language=en-US&query=#{search}%20"
+    json = get_results(uri)
     json[:results]
+  end
+
+  def self.get_results(uri)
+    conn = Faraday.new(url: 'https://api.themoviedb.org')
+    results = conn.get(uri)
+    JSON.parse(results.body, symbolize_names: true)
   end
 end
