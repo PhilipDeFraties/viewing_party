d335e6a887991baeb5b23d1ec611263433a727c9
oxalisviolacea <hannadavis09@gmail.com>
2020-12-09 14:54:34 -0700

    Refactor find_parties method
    Co-authored-by: Greg Mitchell <g2mitchell3003@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,6 @@ class User < ApplicationRecord
     array << Party.where(id: PartyGuest.where(user_id: id).pluck(:party_id))
     array << parties
     array.flatten
-    # Party.where(user_id: id).or(Party.where(id: PartyGuest.where(user_id: id).pluck(:party_id)))
   end
 
   def create_friendships(current_user_id, new_friend_id)
