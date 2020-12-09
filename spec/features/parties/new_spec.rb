04606e1dccbed1f3c10a93d4355b78980ece6cdd
PhilipDeFraties <Philipjfry338@gmail.com>
2020-12-08 20:38:38 -0700

    Adds feature test for creating a viewing party without inviting friends, adds a few more checks in the other party create tests

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe "New party page" do
       expect(page).to have_content('Please login.')
     end
   end
-  
+
   describe "As a registered user when I visit a movies details page" do
     before :each do
       @user_1 = create :user
       @user_2 = create :user
       Friendship.create(user_id: @user_1.id, friend_id: @user_2.id)
-      
+
 
       visit '/'
       fill_in :email, with: @user_1.email
@@ -33,8 +33,12 @@ RSpec.describe "New party page" do
         find(:css, "#party_guests_#{@user_2.id}[value='#{@user_2.id}']").set(true)
         click_on 'Save'
         expect(current_path).to eq(dashboard_path)
+        expect(Movie.all.count).to eq(1)
+        movie = Movie.last
+        expect(movie.title).to eq('Parasite')
         party = Party.last
-        expect(party.party_guests.first.user_id).to eq(@user_2.id)
+        expect(PartyGuest.all.count).to eq(1)
+        expect(party.users).to eq([@user_2])
       end
     end
 
@@ -50,10 +54,28 @@ RSpec.describe "New party page" do
         find(:css, "#party_guests_#{@user_2.id}[value='#{@user_2.id}']").set(true)
         click_on 'Save'
         expect(Movie.all.count).to eq(1)
+        movie = Movie.last
+        expect(movie.title).to eq('Parasite')
       end
     end
 
-    it "if I a negative duration I get an error message" do
+    it "I can create a new viewing party without adding friends" do
+      @movie = Movie.create!(api_id: 291545, title: 'Parasite', logo: "/astKJpagcTTqybiAZ6qpakVqmow.jpg", runtime: 142)
+      VCR.use_cassette('parasite_details') do
+        visit '/movies/291545'
+        click_on 'Create Viewing Party for Movie'
+        expect(current_path).to eq('/viewing-party/new')
+        fill_in :duration, with: '180'
+        fill_in :date, with: Date.today.strftime("%m-%d-%Y")
+        fill_in :time, with: '08:00 PM'
+        # find(:css, "#party_guests_#{@user_2.id}[value='#{@user_2.id}']").set(true)
+        click_on 'Save'
+        expect(Movie.all.count).to eq(1)
+        expect(PartyGuest.all.count).to eq(0)
+      end
+    end
+
+    it "if I enter a negative duration I get an error message" do
       @movie = Movie.create!(api_id: 291545, title: 'Parasite', logo: "/astKJpagcTTqybiAZ6qpakVqmow.jpg", runtime: 142)
       VCR.use_cassette('parasite_details') do
         visit '/movies/291545'
